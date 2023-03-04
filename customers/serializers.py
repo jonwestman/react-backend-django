1d65d184d27e41e7acee1fa731ef2082681c5901
Jon Westman <jon.westman@edu.edugrade.se>
2023-03-04 15:12:23 +0100

    basic 404

--- customers/serializers.py
+++ customers/serializers.py
@@ -4,4 +4,4 @@ from customers.models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = '_all_'
+        fields = '__all__'
